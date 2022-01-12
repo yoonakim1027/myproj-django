a4af38e3cc109b6c85e2fef7c3a7809c76d04cef
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-12 10:18:52 +0900

    모델 생성, admin 등록

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", 'title']
+    search_fields = ['title']
