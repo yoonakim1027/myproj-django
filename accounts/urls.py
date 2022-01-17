d61fbd18ff7638445eaac8a2001f5889d66c3f13
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-01-17 14:41:00 +0900

    JWT token 요청 -> http://localhost:8000/accounts/api/token/ 으로 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,19 @@
+# 아래 token view는 accounts앱 내에 두는 것이 맞습니다! 곧 옮길 것
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 app_name = "accounts"
 
 urlpatterns = []
+
+# 토큰을 발급받는 주소가 바뀌게 됨
+urlpatterns += [
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
+
 
